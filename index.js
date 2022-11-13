b61e6905406dcb4a6a1e09218267b5eef94a4384
rizanb <anshu.aerim@gmail.com>
2022-11-13 19:34:07 +0545

    slight changes to index.js file

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ import dotenv from 'dotenv';
 import authRouter from './routes/authRoutes.js'
 import { requireAuth, checkUser } from "./middleware/authMiddleware.js";
 import cookieParser from 'cookie-parser'
-
 dotenv.config();
 
 const port = process.env.PORT || 4000;
 const app = express();
+
 mongoose.connect(process.env.DB_CONNECTION_STRING,
     {
         useNewUrlParser: true,
@@ -22,9 +22,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api/transactions/', transactionRouter)
 app.use('/api/auth/', authRouter)
-app.get(
-    '/protected', requireAuth, (req, res) => { res.send('inside protected route'); }
-)
+
+app.get('/protected', requireAuth, (req, res) => { res.send('inside protected route'); })
+
 app.get(
     '*',
     () => {
@@ -36,9 +36,3 @@ app.listen(port,
     () => {
         console.log(`listening on port ${port}`)
     });
-
-
-
-
-
-
